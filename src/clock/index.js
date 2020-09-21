dc8e608b226353e5a840e2489fdc7fe92dbe0706
Daniil Grishkin <gddbox@mail.ru>
2020-09-21 20:57:30 +0300

    refactor clock

--- src/clock/index.js
+++ src/clock/index.js
@@ -10,28 +10,68 @@
     const SECONDS_STEP = 360 / 60;
     const INTERVAL_TIME = 1000;
 
-    function rotate(el, value) {
-        el.style.transform = `rotate(${value}deg)`;
+    let d = new Date();
+    let date = {
+        value: d,
+        seconds: d.getSeconds(),
+        minutes: d.getMinutes(),
+        hours: convertHours(d.getHours()),
+
+        getValues: function() {
+            return {
+                hours: this.hours,
+                minutes: this.minutes,
+                seconds: this.seconds++,
+            };
+        },
+
+        update: function () {
+            let d = new Date();
+
+            this.value = d;
+            this.seconds = d.getSeconds();
+            this.minutes = d.getMinutes();
+            this.hours = convertHours(d.getHours());
+
+            return this;
+        },
+    };
+
+    // utils
+    /**
+     * Convert value from 24-hour to 12-hour system
+     * @param {number} hours 
+     * @return {number}
+     */
+    function convertHours(hours) {
+        return hours >= HOURS ? hours - HOURS : hours;
+    }
+
+    /**
+     * Rotate (animated) provided element to provided values
+     * @param {HTMLELement} el 
+     * @param {number} to 
+     */
+    function rotate(el, to) {
+        el.style.transform = `rotate(${to}deg)`;
     }
 
     function update() {
-        const currentD = new Date();
+        const newDate = date.update().getValues();
 
-        const currentHours = currentD.getHours();
-        const hours = currentHours >= HOURS ?
-            currentHours - HOURS :
-            currentHours;
+        const hoursDeg = newDate.hours * HOURS_STEP;
+        const minDeg = newDate.minutes * MINUTES_STEP;
+        const secDeg = newDate.seconds * SECONDS_STEP;
 
-        rotate(hourEl, hours * HOURS_STEP);
-        rotate(minuteEl, currentD.getMinutes() * MINUTES_STEP);
-        rotate(secondEl, currentD.getSeconds() * SECONDS_STEP);
+        rotate(hourEl, hoursDeg);
+        rotate(minuteEl, minDeg);
+        rotate(secondEl, secDeg);
     }
 
-    setTimeout(function() {
-        setInterval(update, INTERVAL_TIME);
-    }, (new Date()).getMilliseconds());
-
-    // init
-    update();
+    // TODO: fix animated rotation from 360 to 0
+    setInterval(
+        (update(), update),
+        INTERVAL_TIME
+    );
 
 })();
